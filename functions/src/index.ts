d41bf8b79cf2fc38b559c6962852c1aae9fb538b
davidgohzk <david.gohzhekai@gmail.com>
2022-05-11 22:21:22 +0800

    added dummy code for functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,41 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
+admin.initializeApp();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
+
+export const addMessage = functions.https.onRequest(
+  async (request, response) => {
+    const original = request.query.text;
+    const writeResult = await admin
+      .firestore()
+      .collection("messages")
+      .add({ original: original });
+    response.json({ result: `Message with ID: ${writeResult.id} added.` });
+  }
+);
+
+// Listens for new messages added to /messages/:documentId/original and creates an
+// uppercase version of the message to /messages/:documentId/uppercase
+export const makeUppercase = functions.firestore
+  .document("/messages/{documentId}")
+  .onCreate((snap, context) => {
+    // Grab the current value of what was written to Firestore.
+    const original = snap.data().original;
+
+    // Access the parameter `{documentId}` with `context.params`
+    functions.logger.log("Uppercasing", context.params.documentId, original);
+
+    const uppercase = original.toUpperCase();
+
+    // You must return a Promise when performing asynchronous tasks inside a Functions such as
+    // writing to Firestore.
+    // Setting an 'uppercase' field in Firestore document returns a Promise.
+    return snap.ref.set({ uppercase }, { merge: true });
+  });
